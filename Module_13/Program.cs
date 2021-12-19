fdc86e5a91e0617686df8d2d21c8d9bade1166ab
Khalmatov <large000@yandex.ru>
2021-12-19 19:29:56 +0300

    Добавьте файлы проекта.

---
+++ Module_13/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
