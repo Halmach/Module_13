6028a4fddc6864fcccc69dff2dfa139a044a00d2
Khalmatov <large000@yandex.ru>
2021-12-19 19:36:02 +0300

    Задание 13.1.4

--- Module_13/Program.cs
+++ Module_13/Program.cs
@@ -6,7 +6,20 @@ namespace Module_13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(checkArray(arr));
+            Array.Reverse(arr);
+            Console.WriteLine(checkArray(arr));
+        }
+
+        private static bool checkArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1]) return true;
+            }
+
+            return false;
         }
     }
 }
