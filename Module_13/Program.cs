443d4cfff158f4f8cecfed394ea061283c7e52fc
Khalmatov <large000@yandex.ru>
2021-12-20 23:00:17 +0300

    Задание 13.2.5

--- Module_13/Program.cs
+++ Module_13/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.IO;
 
 namespace Module_13
 {
@@ -6,10 +8,45 @@ namespace Module_13
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1, 2, 3, 4, 5 };
-            Console.WriteLine(checkArray(arr));
-            Array.Reverse(arr);
-            Console.WriteLine(checkArray(arr));
+            uneducatedListWorkShow();
+            //Console.WriteLine("Введите путь к текстовому файлу");
+            //string path = @"C:\Users\pc\Downloads\cdev_Text.txt";
+            //long wordsCnt = 0;
+            //char[] sepChars = { ' ', '\n', '\r' };
+            //if (File.Exists(path))
+            //{
+            //    try
+            //    {
+            //        var text = File.ReadAllText(path);
+            //        var words = text.Split(sepChars, StringSplitOptions.RemoveEmptyEntries);
+            //        wordsCnt = words.Length;
+            //        //using (StreamReader sr = File.OpenText(path))
+            //        //{
+            //        //    string str = string.Empty;
+            //        //    var temp = new string[] {""};
+
+            //        //    //while ((str = sr.ReadLine()) != null)
+            //        //    //{
+            //        //    //    str = str.Trim();
+            //        //    //    for(int i = 0; i < str.Length; i++)
+            //        //    //    {
+            //        //    //        temp = str.Split(sepChars,StringSplitOptions.RemoveEmptyEntries);
+            //        //    //        wordsCnt += temp.Length;
+            //        //    //    }
+            //        //    //}
+            //        //}
+
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine(e.Message);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Файл по заданному пути не найден");
+            //}
+            //Console.WriteLine($"Количество слов в книге: {wordsCnt}");
         }
 
         private static bool checkArray(int[] arr)
@@ -20,6 +57,42 @@ namespace Module_13
             }
 
             return false;
+        }
+
+        private static void uneducatedListWorkShow()
+        {
+            var months = new[]
+            {
+                "Jan", "Feb", "Mar", "Apr", "May" , "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            };
+
+            var numbers = new[]
+            {
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12
+            };
+
+            var arrList = new ArrayList();
+            for (int i = 0; i < months.Length; i++)
+            {
+                arrList.Add(months[i]);
+                arrList.Add(numbers[i]);
+            }
+
+            foreach (var elem in arrList)
+            {
+                Console.WriteLine(elem);
+            }
         }
     }
 }
